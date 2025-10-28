ce9c9de508f6020f3eb19bf9af90c1770bbc2f68
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-10-28 14:57:29 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
     pass
+n = int(input("Введите кол-во элементов списка: "))
+list = []
+for i in range(n):
+    element = int(input(f"Введите элемент {i + 1}: "))
+    list.append(element)
+positive_num = [x for x in list if x > 0]
+positive_num.sort(reverse=True)
+print(f"Положительные числа по убыванию: {positive_num}")
