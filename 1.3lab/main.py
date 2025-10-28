aaa9c6b7c298dc61f85114840f86b6ca3184b12a
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-10-28 14:26:48 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
     pass
+n = int(input("Введите натуральное число: "))
+result = 0
+for i in range(n + 1):
+    term = ((-1) ** i) * (3 ** i)
+    result += term
+print(f"Сумма: {result}")
