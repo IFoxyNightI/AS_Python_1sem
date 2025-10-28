74d9aaf1e7e53438296cf8039d791acd06d1ca29
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-10-28 14:48:21 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,21 @@
 if __name__ == "__main__":
     pass
+N = int(input("Введите кол-во элементов списка: "))
+print("Введите элементы списка: ")
+my_list = []
+for i in range(N):
+    element = int(input(f"Элемент {i + 1}: "))
+    my_list.append(element)
+K = int(input("Введите число K: "))
+L = int(input("Введите число L: "))
+if not (1 < K <= L <= N) :
+    print("Ошибка: должно выполняться условие: 1<K≤L≤N")
+else:
+    total_sum = 0
+    count = 0
+    for i in range(N):
+        if i + 1 < K or i + 1 > L:
+            total_sum += my_list[i]
+            count += 1
+average = total_sum / count if count > 0 else 0
+print(f"Среднее арифметическое: {average: .2f}")
