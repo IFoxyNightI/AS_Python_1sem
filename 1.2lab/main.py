08e936fa63a9355717b5bd21a3d69e2531527e6e
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-10-28 14:07:42 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
     pass
+A = int(input("Введите число A: "))
+B = int(input("Введите число B: "))
+if A >= B:
+    print("Ошибка - A должно быть меньше B")
+else:
+    for i in range(B-1, A, -1):
+        print(i)
+    N = B - A - 1
+    print(f"Количество чисел = {N}")
