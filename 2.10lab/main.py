b606f744e09b17ac184bafe58a76ba612ec2e5a2
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-11-11 14:28:29 +0300

    Update main.py

--- 2.10lab/main.py
+++ 2.10lab/main.py
@@ -1,2 +1,20 @@
 if __name__ == "__main__":
     pass
+#a)
+try:
+    a = float(input("Первое число: "))
+    b = float(input("Второе число: "))
+    print(a / b)
+except ZeroDivisionError:
+    print("Деление на ноль запрещено")
+#б)
+n = int(input("Введите число: "))
+for i in range(2, n):
+    if n % i == 0:
+        raise Exception("Число не является простым")
+print("Число является простым")
+#в)
+a = float(input("Первое число: "))
+b = float(input("Второе число: "))
+assert b != 0, "Деление на ноль запрещено"
+print(a / b)
