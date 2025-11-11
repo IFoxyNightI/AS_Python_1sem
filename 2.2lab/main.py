0cf7813c4fc984bf82e46e00d343056db99ae427
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-11-11 14:50:19 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
     pass
+def TrianglePS(a) :
+    P = 3 * a
+    S = (3 / 4 * a ** 2)
+    return P, S
+a = float(input("Введите сторону треугольника: "))
+perimeter, area = TrianglePS(a)
+print(f"Периметр: {perimeter}")
+print(f"Площадь: {area}")
