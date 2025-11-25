aab8b88a067b5358120d85f631b1459e495b8872
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-11-25 14:54:49 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,10 @@
 if __name__ == "__main__":
     pass
+def SqrtK(x, k, n):
+    if n == 0:
+        return 1  # y(0) = 1
+    else:
+        y_prev = SqrtK(x, k, n - 1)
+        return y_prev - (y_prev - x / (y_prev ** (k - 1))) / k
+#Пример:
+print(SqrtK(16, 4, 10))
