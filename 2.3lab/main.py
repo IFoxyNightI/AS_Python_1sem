b57b752600cf576d04a23878c726855aa1a1e36f
IFoxyNightI <lisaalisalisa747@gmail.com>
2025-11-25 14:37:44 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -2,3 +2,8 @@ if __name__ == "__main__":
     pass
 def swap_row(k1, k2):
     for(i in range(n)) a[k1][i], a[k2][i] = a[k2][i], a[k1][i]
+#Пример:
+a = [[1, 2], [3, 4]]
+n = 2
+swap_row(0, 1)
+print(a)
